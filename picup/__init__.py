b0b76cf97d959d8bb867d27456c49171c1fcc128
Arvedui <arvedui@posteo.de>
2015-05-25 20:52:30 +0200

    correct log level

--- picup/__init__.py
+++ picup/__init__.py
@@ -29,7 +29,7 @@ from picup.main_window import MainWindow
 
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.ERROR)
 
 
 def main():
