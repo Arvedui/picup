b6cab650f7e66ea3e3bdb7e98e54b9d8580b2920
Arvedui <arvedui@posteo.de>
2015-06-28 16:31:17 +0200

    removed license block from every source file, a licence file in the repo is enough imho

--- picup/__init__.py
+++ picup/__init__.py
@@ -1,21 +1,7 @@
 # -*- coding:utf8 -*-
-######################## BEGIN LICENSE BLOCK ########################
-# picup - small gui tool for uploading pictures to picflash
-# Copyright (C) 2014  Arvedui
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; version 2 of the License.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software Foundation,
-# Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
-######################### END LICENSE BLOCK #########################
+"""
+main module does some logging and qt setup and hands over to MainWindow
+"""
 
 from picup.globals import __version__
 
@@ -29,10 +15,16 @@ from picup.main_window import MainWindow
 
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.ERROR)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.ERROR
+)
 
 
 def main():
+    """
+    app main function
+    """
     app = QApplication(sys.argv)
     app.setOrganizationName('Arvedui')
     app.setApplicationName('picup')
--- picup/dialogs/show_links.py
+++ picup/dialogs/show_links.py
@@ -1,21 +1,5 @@
 # -*- coding:utf8 -*-
-######################## BEGIN LICENSE BLOCK ########################
-# picup - small gui tool for uploading pictures to picflash
-# Copyright (C) 2014  Arvedui
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; version 2 of the License.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software Foundation,
-# Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
-######################### END LICENSE BLOCK #########################
+
 from __future__ import unicode_literals
 
 from os import path
